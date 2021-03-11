e961fb766f025e2679b5331504ffa7cd048f5431
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2021-03-11 15:45:11 +0000

    Update the chunked and sleep.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstiersqstool/service/CalculationRequestService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstiersqstool/service/CalculationRequestService.kt
@@ -33,7 +33,7 @@ class CalculationRequestService(
     val now = LocalDateTime.now()
     crns
       .filter { crn -> calculationRequestRepository.findFirstByCrn(crn).also { log.info("Checking for Duplicate: $crn") } == null }
-      .chunked(10000)
+      .chunked(1000)
       .map {
         it.map { crn -> CalculationRequestEntity(crn = crn, created = now) }
       }
@@ -48,17 +48,18 @@ class CalculationRequestService(
       val crnRequests = calculationRequestRepository.findAllByProcessedIsNull(PageRequest.of(0, PAGE_SIZE)).also {
         log.info("Fetched ${it.numberOfElements} calculation requests")
       }
-      sendMessagesFromList(crnRequests.get().toList().map { it.crn }).forEach { updateSentEntity(it.id, it.messageId) }
+      crnRequests.get().toList().map { it.crn }.chunked(5).forEach { crns ->
+        sendMessagesFromList(crns).forEach { updateSentEntity(it.id, it.messageId) }
+        Thread.sleep(1000L)
+      }
     } while (crnRequests.totalPages > 1)
   }
 
-  fun sendMessagesFromList(requests: Collection<String>) : List<SendMessageBatchResultEntry> =
-    requests.chunked(10)
-      .flatMap { messages ->
-      val messageRequests = messages.map { SendMessageBatchRequestEntry(it, "{ \"Message\" : \"{\\\"crn\\\": \\\"$it\\\"}\" }") }
-      log.info("Sending messages $requests")
-      amazonSQS.sendMessageBatch(SendMessageBatchRequest(queueUrl, messageRequests)).successful
-    }
+  fun sendMessagesFromList(requests: Collection<String>) : List<SendMessageBatchResultEntry> {
+    val messageRequests = requests.map { SendMessageBatchRequestEntry(it, "{ \"Message\" : \"{\\\"crn\\\": \\\"$it\\\"}\" }") }
+    log.info("Sending messages $requests")
+    return amazonSQS.sendMessageBatch(SendMessageBatchRequest(queueUrl, messageRequests)).successful
+  }
 
   fun uploadCsvFile(file: MultipartFile) {
     throwIfFileEmpty(file)
@@ -79,11 +80,11 @@ class CalculationRequestService(
   }
 
   private fun createCSVToBean(fileReader: BufferedReader?): CsvToBean<ActiveCrn> =
-    CsvToBeanBuilder<ActiveCrn>(fileReader)
-      .withType(ActiveCrn::class.java)
-      .withSkipLines(1)
-      .withIgnoreLeadingWhiteSpace(true)
-      .build()
+          CsvToBeanBuilder<ActiveCrn>(fileReader)
+                  .withType(ActiveCrn::class.java)
+                  .withSkipLines(1)
+                  .withIgnoreLeadingWhiteSpace(true)
+                  .build()
 
   private fun closeFileReader(fileReader: BufferedReader?) {
     try {
@@ -94,20 +95,21 @@ class CalculationRequestService(
   }
 
   private fun updateSentEntity(crn: String, messageId: String) =
-    calculationRequestRepository.findFirstByCrn(crn)?.let {
-      it.processed = LocalDateTime.now()
-      it.messageId = messageId
-      calculationRequestRepository.save(it)
-    }.also {
-      log.info("Updated Calculation request ${it?.crn}")
-      log.info(it.toString())
-    }
+          calculationRequestRepository.findFirstByCrn(crn)?.let {
+            it.processed = LocalDateTime.now()
+            it.messageId = messageId
+            calculationRequestRepository.save(it)
+          }.also {
+            log.info("Updated Calculation request ${it?.crn}")
+            log.info(it.toString())
+          }
 
   companion object {
-    private const val PAGE_SIZE = 1000
-    private val log = LoggerFactory.getLogger(CalculationRequestService::class.java)
-  }
+  private const val PAGE_SIZE = 1000
+  private val log = LoggerFactory.getLogger(CalculationRequestService::class.java)
 }
+
+  }
 
 data class ActiveCrn(
   @CsvBindByPosition(position = 0)
