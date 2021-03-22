97e5368ed39d94f00145e4a22a3ed87a8b9bead4
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-03-22 13:11:33 +0000

    MAS-278 test for sending a file to the queue

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiersqstool/SendCrnsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiersqstool/SendCrnsTest.kt
@@ -12,9 +12,13 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.MediaType
 import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.client.MultipartBodyBuilder
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
 import reactor.core.publisher.Mono
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -51,5 +55,24 @@ class SendCrnsTest {
     await untilCallTo { getNumberOfMessagesCurrentlyOnEventQueue(eventAwsSqsClient, eventQueueUrl) } matches { it == 2 }
     val message = eventAwsSqsClient.receiveMessage(ReceiveMessageRequest(eventQueueUrl))
     assertThat(message.messages.get(0).body).contains("CRN1")
+  }
+
+  @Test
+  fun `sends file of CRNs to queue`() {
+
+    val multipartBodyBuilder = MultipartBodyBuilder()
+    multipartBodyBuilder.part("file", ClassPathResource("fixtures/file/crns.csv"))
+      .contentType(MediaType.MULTIPART_FORM_DATA)
+
+    webTestClient.post()
+      .uri("/file")
+      .body(BodyInserters.fromMultipartData(multipartBodyBuilder.build()))
+      .exchange()
+      .expectStatus()
+      .isOk
+
+    await untilCallTo { getNumberOfMessagesCurrentlyOnEventQueue(eventAwsSqsClient, eventQueueUrl) } matches { it == 3 }
+    val message = eventAwsSqsClient.receiveMessage(ReceiveMessageRequest(eventQueueUrl))
+    assertThat(message.messages.get(0).body).contains("CRNFromFile")
   }
 }
