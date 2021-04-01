b807fed6c009ada12207bd2c84ce30acb1fb9eca
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-04-01 13:17:19 +0100

    MAS-384 send telemetry event when taking CRN off dead letter queue

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstiersqstool/service/QueueAdminService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstiersqstool/service/QueueAdminService.kt
@@ -4,6 +4,8 @@ import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.model.DeleteMessageRequest
 import com.amazonaws.services.sqs.model.PurgeQueueRequest
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest
+import com.google.gson.Gson
+import com.microsoft.applicationinsights.TelemetryClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Service
 class QueueAdminService(
   private val eventAwsSqsClient: AmazonSQS,
   private val eventAwsSqsDlqClient: AmazonSQS,
+  private val gson: Gson,
+  private val telemetryClient: TelemetryClient,
   @Value("\${offender-events.sqs-queue}") private val eventQueueUrl: String,
   @Value("\${offender-events-dlq.sqs-queue}") private val eventDlqUrl: String
 
@@ -28,6 +32,9 @@ class QueueAdminService(
     repeat(dlqMessageCount) {
       eventAwsSqsDlqClient.receiveMessage(ReceiveMessageRequest(eventDlqUrl).withMaxNumberOfMessages(1)).messages
         .forEach { msg ->
+          val message = gson.fromJson(msg.body, SQSMessage::class.java).Message
+          val crn = gson.fromJson(message, TierChangeEvent::class.java).crn
+          telemetryClient.trackEvent("TierCRNFromDeadLetterQueue", mapOf("crn" to crn), null)
           eventAwsSqsClient.sendMessage(eventQueueUrl, msg.body)
           eventAwsSqsDlqClient.deleteMessage(DeleteMessageRequest(eventDlqUrl, msg.receiptHandle))
         }
@@ -42,4 +49,13 @@ class QueueAdminService(
     eventAwsSqsDlqClient.getQueueAttributes(eventDlqUrl, listOf("ApproximateNumberOfMessages"))
       .attributes["ApproximateNumberOfMessages"]
       ?.toInt() ?: 0
+
+  data class SQSMessage(
+    val Message: String,
+    val MessageId: String
+  )
+
+  data class TierChangeEvent(
+    val crn: String
+  )
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiersqstool/TransferQueueTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiersqstool/TransferQueueTest.kt
@@ -2,11 +2,13 @@ package uk.gov.justice.digital.hmpps.hmppstiersqstool
 
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.model.PurgeQueueRequest
+import com.microsoft.applicationinsights.TelemetryClient
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilCallTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
@@ -29,6 +31,9 @@ class TransferQueueTest {
   @Autowired
   lateinit var webTestClient: WebTestClient
 
+  @Autowired
+  lateinit var telemetryClient: TelemetryClient
+
   @Value("\${offender-events-dlq.sqs-queue}")
   lateinit var eventDlqQueueUrl: String
 
@@ -50,5 +55,16 @@ class TransferQueueTest {
       .isOk
     await untilCallTo { getNumberOfMessagesCurrentlyOnDeadLetterQueue(eventAwsSqsDlqClient, eventDlqQueueUrl) } matches { it == 0 }
     await untilCallTo { getNumberOfMessagesCurrentlyOnEventQueue(eventAwsSqsClient, eventQueueUrl) } matches { it == 1 }
+  }
+
+  @Test
+  fun `writes telemetry when moving message`() {
+    putMessageOnDlq(eventAwsSqsDlqClient, eventDlqQueueUrl)
+    webTestClient.get().uri("/transfer")
+      .exchange()
+      .expectStatus()
+      .isOk
+
+    verify(telemetryClient).trackEvent("TierCRNFromDeadLetterQueue", mapOf("crn" to "X373878"), null)
   }
 }
